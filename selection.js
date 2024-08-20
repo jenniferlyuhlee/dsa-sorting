91a925b5a4ecdfbc89c2ccc43b8394d6eb0ec92a
jenniferlyuhlee <jenniferlyuhlee@gmail.com>
2024-08-20 00:51:35 -0700

    dsa sorting exercises

---
+++ selection.js
@@ -0,0 +1,18 @@
+function selectionSort(arr) {
+    for (let i=0; i< arr.length; i++){
+        let minIdx = i;
+        for (let j = i+1; j<arr.length; j++){
+            if(arr[j] < arr[minIdx]){
+                minIdx = j
+            }
+        }
+        if (minIdx != i){
+            let temp = arr[i];
+            arr[i] = arr[minIdx];
+            arr[minIdx] = temp;
+        }
+    }
+    return arr;
+}
+
+module.exports = selectionSort;
